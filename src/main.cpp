c2e5a98e33fae8045e6565674f2756333acae622
Minh Cuong Vu <noedigsti@gmail.com>
2022-07-19 05:21:04 +1000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Adder.h"
+
+void print_hello()
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << add(1.0f, 2.0f) << std::endl;
+}
+
+int main(int argc, const char **argv)
+{
+    print_hello();
+    return 0;
+}
